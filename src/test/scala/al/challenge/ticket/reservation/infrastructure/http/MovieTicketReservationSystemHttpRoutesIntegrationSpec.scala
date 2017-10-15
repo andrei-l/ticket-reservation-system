a2d8df0f36bfaa566e2d7a5334077c75d4eabd77
Andrei Loginov <loginov.andrei@gmail.com>
2017-10-15 21:26:00 +0100

    - added integration tests for edge scenarios  - fixed bugs in rest endpoints

--- src/test/scala/al/challenge/ticket/reservation/infrastructure/http/MovieTicketReservationSystemHttpRoutesIntegrationSpec.scala
+++ src/test/scala/al/challenge/ticket/reservation/infrastructure/http/MovieTicketReservationSystemHttpRoutesIntegrationSpec.scala
@@ -6,49 +6,64 @@ import akka.http.scaladsl.testkit.ScalatestRouteTest
 import al.challenge.ticket.reservation.infrastructure.actor.ActorsModule
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.language.implicitConversions
+
 class MovieTicketReservationSystemHttpRoutesIntegrationSpec extends WordSpec
   with Matchers
   with ScalatestRouteTest
   with HttpRoutesModule
   with ActorsModule {
 
+  private final val CreateMovieRequestEntity = HttpEntity(MediaTypes.`application/json`,
+    s"""
+             {
+                 "imdbId": "tt0111161",
+                 "screenId": "screen_123456",
+                 "movieTitle": "The Shawshank Redemption",
+                 "availableSeats": 1
+             }
+         """
+  )
+
+  private final val ReserveSeatRequestEntity = HttpEntity(MediaTypes.`application/json`,
+    """
+           {
+               "imdbId": "tt0111161",
+               "screenId": "screen_123456"
+           }
+       """
+  )
 
   "Application" should {
-    "register movie via Put /movies/register" in {
-      val requestEntity = HttpEntity(MediaTypes.`application/json`,
-        s"""
-               {
-                   "imdbId": "tt0111161",
-                   "screenId": "screen_123456",
-                   "movieTitle": "The Shawshank Redemption",
-                   "availableSeats": 1
-               }
-           """
-      )
-
-      Put("/movies/register", requestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
+    "register movie via PUT /movies/register" in {
+      Put("/movies/register", CreateMovieRequestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
         status shouldBe Created
       }
     }
 
-    "reserve a seat for a movie via POST /movies/reserve-seat" in {
-      val requestEntity = HttpEntity(MediaTypes.`application/json`,
-        """
-               {
-                   "imdbId": "tt0111161",
-                   "screenId": "screen_123456"
-               }
-           """
-      )
+    "fail to register duplicate movie via PUT /movies/register" in {
+      Put("/movies/register", CreateMovieRequestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
+        responseAs[String] shouldBe """{"msg":"Movie already exist"}"""
+        status shouldBe BadRequest
+      }
+    }
 
-      Post("/movies/reserve-seat", requestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
+    "reserve a seat for a movie via POST /movies/reserve-seat" in {
+      Post("/movies/reserve-seat", ReserveSeatRequestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
         status shouldBe OK
+      }
+    }
+
+    "fail to reserve a seat for a movie as there are no seats left via POST /movies/reserve-seat" in {
+      Post("/movies/reserve-seat", ReserveSeatRequestEntity) ~> httpRoute.movieTicketSystemRoute ~> check {
+        responseAs[String] shouldBe """{"msg":"All tickets have been already reserved"}"""
+        status shouldBe BadRequest
       }
     }
 
     "reserve a seat for a movie via GET /movies/get-movie-info?imdbId=x&screenId=y" in {
       Get(s"/movies/get-movie-info?imdbId=tt0111161&screenId=screen_123456") ~> httpRoute.movieTicketSystemRoute ~> check {
-        responseAs[String].replaceAll("\\s+", "") shouldBe
+        responseAs[String].noSpaces shouldBe
           """
                {
                     "imdbId": "tt0111161",
@@ -57,10 +72,16 @@ class MovieTicketReservationSystemHttpRoutesIntegrationSpec extends WordSpec
                     "availableSeats": 1,
                     "reservedSeats": 1
                 }
-          """.replaceAll("\\s+", "")
+          """.noSpaces
         status shouldBe OK
       }
     }
+  }
+
+  private implicit def stringToRichString(s: String): RichString = RichString(s)
+
+  private case class RichString(s: String) {
+    def noSpaces: String = s.replaceAll("\\s+", "")
   }
 
 }
